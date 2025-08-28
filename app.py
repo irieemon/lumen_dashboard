a9ca4b1499c12a9a35adbddddd76a73e214928d7
irieemon <sean@lakehouse.net>
2025-08-28 17:25:02 -0400

    Load Streamlit component lib and set initial iframe height

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def main() -> None:
     with index_path.open(encoding="utf-8") as f:
         html = f.read()
 
-    st.components.v1.html(html, height=0, scrolling=False)
+    st.components.v1.html(html, height=1000, scrolling=False)
 
 
 if __name__ == "__main__":
